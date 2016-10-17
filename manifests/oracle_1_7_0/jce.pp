94fbd1557e98d8982bbf9d0a4df022948df3b98c
Alwyn Kik <alwyn@firelay.com>
2016-10-17 12:39:08 +0100

    Added explicit permissions to JCE.

--- manifests/oracle_1_7_0/jce.pp
+++ manifests/oracle_1_7_0/jce.pp
@@ -3,14 +3,20 @@
 #
 #
 class java::oracle_1_7_0::jce (
-  $home    = $java::oracle_1_7_0::params::home
+    $home    = $java::oracle_1_7_0::params::home
 ) inherits java::oracle_1_7_0::params {
 
-  file { "${home}/jre/lib/security/local_policy.jar" :
-    source => 'puppet:///modules/java/UnlimitedJCEPolicy/local_policy.jar',
-  }
+    file { "${home}/jre/lib/security/local_policy.jar" :
+        source  => 'puppet:///modules/java/UnlimitedJCEPolicy/local_policy.jar',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
 
-  file { "${home}/jre/lib/security/US_export_policy.jar" :
-    source => 'puppet:///modules/java/UnlimitedJCEPolicy/US_export_policy.jar',
-  }
+    file { "${home}/jre/lib/security/US_export_policy.jar" :
+        source  => 'puppet:///modules/java/UnlimitedJCEPolicy/US_export_policy.jar',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
 }
